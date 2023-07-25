17e6aad429e8fef9c7c003018112a1086c151b41
seonghyeon <dolla_@naver.com>
2023-07-25 22:25:45 +0900

    refactor: change project name

--- src/main/java/com/example/batchfront/config/socket/back/BatchWebSocketHandler.java
+++ src/main/java/com/example/batchfront/config/socket/back/BatchWebSocketHandler.java
@@ -1,9 +1,6 @@
 package com.example.batchfront.config.socket.back;
 
 import com.example.batchfront.config.socket.front.VueWebSocketHandler;
-import com.example.batchfront.dto.SocketMessage;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -18,17 +15,18 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 public class BatchWebSocketHandler extends TextWebSocketHandler {
 
     private final VueWebSocketHandler vueWebSocketHandler;
-    private final ObjectMapper objectMapper;
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
         log.info("Created new connection: BatchSocketSession[id:{}]", session.getId());
     }
 
+    /**
+     * batch 서버에서 받은 메시지를 vue 서버로 전달
+     */
     @Override
-    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws JsonProcessingException {
-        SocketMessage sockMessageSpec = objectMapper.readValue(message.getPayload().toString(), SocketMessage.class);
-        vueWebSocketHandler.sendMessage(objectMapper.writeValueAsString(sockMessageSpec));
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) {
+        vueWebSocketHandler.sendMessage(message.getPayload().toString());
     }
 
 }

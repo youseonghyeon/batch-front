354ea8b83aa833e108e3136cc4ac05b040bdb51b
seonghyeon <dolla_@naver.com>
2023-07-26 09:48:07 +0900

    fix: refix cors mapping

--- src/main/java/com/example/batchfront/config/WebConfig.java
+++ src/main/java/com/example/batchfront/config/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:80")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST")
                 .allowedHeaders("*")
                 .allowCredentials(true)

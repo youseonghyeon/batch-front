9eefd1a113d5ca72ab728be6023949df52074825
seonghyeon <dolla_@naver.com>
2023-07-26 10:01:00 +0900

    fix : cors mapping

--- src/main/java/com/example/batchfront/config/WebConfig.java
+++ src/main/java/com/example/batchfront/config/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOriginPatterns("http://www.sideproject.site:80", "http://localhost:5672", "*")
                 .allowedMethods("GET", "POST")
                 .allowedHeaders("*")
                 .allowCredentials(true)
